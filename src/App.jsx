c48510414e0bf166fbd81eb2ce671e8c515454fc
Sohaib Hussain Shah <155159661+SyedSohaib456@users.noreply.github.com>
2024-08-15 00:22:38 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './components/Header'
+import Header from './components/Header';
 import Exchange from "./components/Exchange";
-import Home from './components/Home.jsx'
+import Home from './components/Home.jsx';
 import Coins from "./components/Coins.jsx";
 import CoinDetails from "./components/CoinDetails.jsx";
 import Footer from "./components/Footer.jsx";
+
 function App() {
   return (
     <>
       <BrowserRouter basename="/Cryptexia">
-         <Header/>
+        <Header />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/exchange" element={<Exchange/>} />
-          <Route path="/coins" element={<Coins/>} />
-          <Route path="/coin/:id" element={<CoinDetails />} />       
+          <Route path="/" element={<Home />} />
+          <Route path="/exchange" element={<Exchange />} />
+          <Route path="/coins" element={<Coins />} />
+          <Route path="/coin/:id" element={<CoinDetails />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
   );
