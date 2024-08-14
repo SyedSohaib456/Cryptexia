89324fc4323d7b0b1a48abba301a0239f590aeb5
Sohaib Hussain Shah <155159661+SyedSohaib456@users.noreply.github.com>
2024-08-15 00:01:27 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Cryptexia/', // Ensure this matches your repository name
+});
